7fd94cf50d98f9af141eca919f5b22f3ad0367e5
recursean <seanmcshane96@gmail.com>
2024-04-07 19:53:15 -0400

    selected files now open in read only view

--- src/extension.ts
+++ src/extension.ts
@@ -1,85 +1,130 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
 import * as JSZip from 'jszip';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+/**
+ * 
+ *  SECTION: global vars
+ * 
+ */
+
+// scheme used when opening files in editor view
+const scheme = 'jar-viewer-and-decompiler';
+
+// global var used to share contents of file with editor view
+var fileContents = "";
+
+/**
+ * 
+ *  SECTION: Extension activation and deactivation
+ * 
+ */
+
+/**
+ * Main activation method for extension. Called when first interaction
+ * with extension occurs. 
+ * @param context 
+ */
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "jar-viewer-and-decompiler" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('jar-viewer-and-decompiler.viewJarContents', viewJarContents);
+	context.subscriptions.push(vscode.commands.registerCommand('jar-viewer-and-decompiler.viewJarContents', viewJarContents));
+	context.subscriptions.push(vscode.commands.registerCommand('jar-viewer-and-decompiler.openFile', openFile));
 
-	context.subscriptions.push(disposable);
-
-	let disposable2 = vscode.commands.registerCommand('jar-viewer-and-decompiler.openFile', openFile);
-
-    context.subscriptions.push(disposable2);
+	// Prepare and register document provider for opening files 
+	// in editor.
+	const provider = new TextDocumentContentProvider();
+    context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(scheme, provider));
 }
 
-// This method is called when your extension is deactivated
+/**
+ * This method is called when extension is deactivated.
+ */
 export function deactivate() {}
 
-// Parse and display contents of JAR file
+/**
+ * 
+ *  SECTION: Extension commands registered in activate()
+ * 
+ */
+
+/**
+ * Parse and display contents of JAR file. Called when user
+ * right clicks on .jar file in file explorer and selects
+ * option to launch this extension.
+ * 
+ * @param uri file that was selected to be viewed
+ */
 function viewJarContents(uri: vscode.Uri) {
 	if (!uri) {
 		vscode.window.showWarningMessage('No JAR file selected.');
 		return;
 	}
 
-	// Now you have the URI of the selected JAR file, you can work with it
-	const jarFilePath = uri.fsPath;
-	console.log(`Selected JAR File: ${jarFilePath}`);
+	// bring this extension into focus
+	vscode.commands.executeCommand('workbench.view.extension.jarViewer').then(() => {});
 
-	// You can then pass this path to your logic that handles the opening and displaying of the JAR contents
-	// await openAndDisplayJarContents(jarFilePath);
-
-	// Bring the JAR Explorer view into focus
-	vscode.commands.executeCommand('workbench.view.extension.jarViewer').then(() => {
-		// Optionally, you can also select a specific item in your custom view here
-	});
-
+	// build content to display in extension view 
 	const jarContentProvider = new JarContentProvider(uri);
     vscode.window.registerTreeDataProvider('jarContents', jarContentProvider);
 }
 
-async function openFile(filePath: string, jarFile: JSZip) {
+/**
+ * Called when a file in the jar file is selected to be opened.
+ * This will open a local copy of the file in the editor view. 
+ * 
+ * @param filePath path to selected file in jar
+ * @param jarFile JSZip object containing selected jar file contents
+ * @param jarFileName name of jar file
+ */
+async function openFile(filePath: string, jarFile: JSZip, jarFileName: string) {
 	try {
-		// Convert the file path to a Uri
-		// const fileUri = vscode.Uri.file(filePath);
-		// console.log("opening " + fileUri);
-		console.log("opening " + filePath);
-
 		var file = jarFile.file(filePath);
 		if(file) {
-			var fileContents = await file.async("string");
-			const document = await vscode.workspace.openTextDocument({
-				content: fileContents,
-				language: "plaintext"
-			});
+			// read contents of selected file
+			fileContents = await file.async("string");
 
-			// Show the text document in a VS Code editor
-			await vscode.window.showTextDocument(document, { preview: true, preserveFocus: true });
+			var parts = filePath.split(".");
+			// was a java class file selected?
+			if(parts[parts.length - 1] === 'class') {
+				// Retrieve the path to the CFR JAR file from the extension settings
+				const cfrPath = vscode.workspace.getConfiguration().get<string>('jar-viewer-and-decompiler.cfrPath');
+				
+				// Construct the Java command
+				// const command = `java -jar ${cfrPath} ${classFilePath}`;
+			
+				// cp.exec(command, (error, stdout, stderr) => {
+				// 	if (error) {
+				// 		console.error(`exec error: ${error}`);
+				// 		return vscode.window.showErrorMessage('Decompilation error: ' + error.message);
+				// 	}
+			
+				// 	// Handle the decompiled Java code in `stdout`
+				// 	console.log(`Decompiled code: ${stdout}`);
+			
+				// 	// Optionally, show the decompiled code in a new VS Code editor tab
+				// 	vscode.workspace.openTextDocument({ content: stdout, language: 'java' }).then(doc => {
+				// 		vscode.window.showTextDocument(doc);
+				// 	});
+				// });
+			}
+			else {
+				// show contents of file in editor viewer
+				const uri = vscode.Uri.parse(`${scheme}:///${jarFileName}/${filePath}`);
+				const doc = await vscode.workspace.openTextDocument(uri); 
+				await vscode.window.showTextDocument(doc, { preview: true });
+			}
 		}
-
-		// Open the text document referred by the Uri
-		// const document = await vscode.workspace.openTextDocument(fileUri);
-
 	} catch (error) {
 		vscode.window.showErrorMessage(`Could not open file: ${error}`);
 	}
 }
-// async function openAndDisplayJarContents(jarFilePath: string) {
-//     // Implement the logic to read and display the contents of the JAR file
-//     // This could involve parsing the JAR file, decompiling classes, etc.
-// }
+
+/**
+ * 
+ *  SECTION: Classes
+ * 
+ */
 
 /**
  * Class responsible for the file directory tree of the JAR file in
@@ -142,10 +187,21 @@ class JarContentProvider implements vscode.TreeDataProvider<JarEntry> {
 		)];
     }
 
+	/**
+	 * Return files and directories in the selected 
+	 * directory. This method will only ever be called 
+	 * for collapsable entries.
+	 * 
+	 * @param path path to directory that was selected
+	 * @returns 
+	 */
     private getEntries(path: string): JarEntry[] {
+		// entries that will be returned
 		var entries: JarEntry[] = [];
 		
+		// node in graph for selected directory
 		var n: GraphNode | undefined;
+
 		// Determine if we are operating on root of jar
 		if(path === this.jarFileName) {
 			n = this.jarMap.get(this.jarFilePath);
@@ -155,25 +211,31 @@ class JarContentProvider implements vscode.TreeDataProvider<JarEntry> {
 		}
 		
 		if(n) {
+			// create local vars for sharing in arguments to openFile
 			var jarFilelocal = this.jarFile;
+			var jarFileNameLocal = this.jarFileName;
+
+			// create an entry for each child of selected directory
 			n.children.forEach(function (child) {
+				// create collapsable entry for directories
 				if(child.dir) {
 					const entry = new JarEntry(
-						child.fileName, // Label to display
+						child.fileName, 
 						child.filePath,
-						vscode.TreeItemCollapsibleState.Collapsed // Indicates that it can be expanded to show child items
+						vscode.TreeItemCollapsibleState.Collapsed 
 					);
 					entries.push(entry);
 				}
+				// create openable entries for files
 				else {
 					const entry = new JarEntry(
-						child.fileName, // Label to display
+						child.fileName,
 						child.filePath,
 						vscode.TreeItemCollapsibleState.None,
 						{
-							command: 'jar-viewer-and-decompiler.openFile', // Command to execute when this item is clicked
-							title: "Open File", // Title of the command, not used in the tree view but required
-							arguments: [child.filePath, jarFilelocal] // Arguments to pass to the command handler
+							command: 'jar-viewer-and-decompiler.openFile', 
+							title: "Open File",
+							arguments: [child.filePath, jarFilelocal, jarFileNameLocal] 
 						}
 					);
 					entries.push(entry);
@@ -189,7 +251,16 @@ class JarContentProvider implements vscode.TreeDataProvider<JarEntry> {
         this._onDidChangeTreeData.fire(null);
     }
 
-	async parseJarFile() {
+	/**
+	 * Use JSZip to loop through contents of jar file and build
+	 * graph representation of contents. Files and directories are 
+	 * represented by nodes. Each node can have 0 or more children 
+	 * nodes. To store the graph, a hash map is used where the key
+	 * is the name of the file/dir and the value is the node. 
+	 * 
+	 * This is done once when a jar file is first selected.
+	 */
+	private async parseJarFile() {
 		// Create root node for jar file
 		this.jarMap.set(this.jarFilePath, new GraphNode(this.jarFileName, true));
 
@@ -210,12 +281,14 @@ class JarContentProvider implements vscode.TreeDataProvider<JarEntry> {
 				var parts = zipEntry.name.split("/");
 				// Is this entry at root level of jar?
 				if( (parts.length === 2 && zipEntry.dir) || (parts.length === 1) ) {
+					// add current node to children of root node
 					var root = this.jarMap.get(this.jarFilePath);
 					if(root) {
 						root.children.push(n);
 					}
 				}
 				else {
+					// get parent node and add current node as a child
 					var parent = parts.splice(0, parts.length - 1).join("/") + "/";
 					var parentNode = this.jarMap.get(parent);
 					if(parentNode) {
@@ -225,21 +298,56 @@ class JarContentProvider implements vscode.TreeDataProvider<JarEntry> {
 			});
 		} catch (error) {
 			console.error(`Failed to parse JAR file: ${error}`);
-			// vscode.window.showErrorMessage(`Failed to parse JAR file: ${error.message}`);
+			vscode.window.showErrorMessage(`Failed to parse JAR file: ${error}`);
 		}
 	}
 }
 
+/**
+ * Represents a file or directory in jar content directory view.
+ */
+class JarEntry extends vscode.TreeItem {
+    constructor(
+		// name to display in directory view
+        public readonly label: string,
+
+		// full path to file or dir in jar file
+        public readonly path: string,
+
+		// none if file, collapsed if dir
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+
+		// command to execute when this entry is selected
+		public readonly command?: vscode.Command
+    ) {
+        super(label, collapsibleState);
+    }
+}
+
+/**
+ * Contents of jar is stored in a graph data structure used to easily
+ * display the contents in the directory tree view. Each node is a 
+ * file or directory in the jar and can have 0 or more children nodes. 
+ */
 class GraphNode {
+	// name of file or dir
 	fileName: string;
+
+	// full path to file or dir in jar
 	filePath: string;
+
+	// files or dirs one level deeper than this node
 	children: GraphNode[];
+
+	// true if directory, false if file
 	dir: boolean;
+
 	constructor(filePath: string, dir: boolean) {
 		this.filePath = filePath;
 		this.children = [];
 		this.dir = dir;
 		this.fileName = "";
+
 		// Parse file name
 		var parts = this.filePath.split("/");
 		if(parts.length > 0) {
@@ -253,19 +361,16 @@ class GraphNode {
 	}
 }
 
-class JarEntry extends vscode.TreeItem {
-    constructor(
-		// Add a command that is executed when the tree item is activated
-        public readonly label: string,
-        public readonly path: string,
-        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-		public readonly command?: vscode.Command
-		// ?: vscode.Command = {
-		// 	command: 'yourExtension.openFile', // The command ID
-		// 	title: '', // Title is not used here but is required
-		// 	arguments: [/* pass necessary arguments here, e.g., the file path */]
-		// }
-    ) {
-        super(label, collapsibleState);
+/**
+ * This class is used when vscode.workspace.openTextDocument(uri) is called
+ * to open a selected file from the jar file. 
+ */
+class TextDocumentContentProvider implements vscode.TextDocumentContentProvider {
+    onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+    onDidChange = this.onDidChangeEmitter.event;
+
+    provideTextDocumentContent(uri: vscode.Uri): string {
+        // return most recent file that was read 
+        return fileContents;
     }
 }
